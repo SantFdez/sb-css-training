d96cac3ec69f61eac97887c481c1c28cfc8afbea
Santiago Fernández <sannt43@gmail.com>
2023-09-04 17:59:58 -0500

    feat: navigation bar

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,29 @@
-const menu_toggle = document.querySelector("#mobile-menu-toggle");
-menu_toggle.addEventListener("click", () => {
-    console.log("click")
-})
+const maybeQuery = (fn, ...args) =>
+  [...document.querySelectorAll(...args)].slice(0, 1).forEach(fn);
+
+maybeQuery(
+  (btn) =>
+    btn.addEventListener("click", () => {
+      const ariaAttr = "aria-expanded";
+      const isExpanded = btn.getAttribute(ariaAttr) == "true";
+      btn.setAttribute(ariaAttr, !isExpanded);
+    }),
+  "#mobile-menu-toggle"
+);
+
+maybeQuery(
+    (btn) =>
+      btn.addEventListener("click", () => {
+        // const classAttr = "class";
+        const bodyElm = document.querySelector("body");
+        // bodyElm.classList.add("light-theme");
+        // const isLight = bodyElm.getAttribute(classAttr) == "light-theme";
+        // if (isLight) {
+            // bodyElm.classList.remove("light-theme");
+        // } else {
+            bodyElm.classList.add("light-theme");
+        // }
+        
+      }),
+    "#theme-button"
+  );
